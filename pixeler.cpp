9835393d8319ab4492d9b215f4b5a3959a4b9984
Rebecca Wardle <mattydwardle@gmail.com>
2021-06-14 01:54:33 +0100

    Moving pixeler to class based solution

--- pixeler.cpp
+++ pixeler.cpp
@@ -5,121 +5,153 @@
 #include <stdexcept>
 
 #include <opencv2/core.hpp>
+#include <opencv2/core/types.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 using namespace cv;
 
 
-class Image 
+// expand on Scalar operations (adding, multiplying, logical operations)
+class pixMat
 {
+    // build off CV2 Mat (allows for show and save operations)
     public:
         int blockH, blockW;
-        short mode;
-        Mat imImg, outImg;
-        Image(std::string image_name, int blockH = 5, int blockW = 5, bool average, bool white)
+        bool average, white;
+        Mat img;
+
+        pixMat(std::string im_name, int blockH = 5, int blockW = 5, bool average = true, bool white = false)
         {
-            // read in image
-            img = imread(image_name, IMREAD_COLOR);
-            
-            if (img.empty())
+            // TODO: unsure what this-> should refer to for imread
+            this->img = imread(im_name, IMREAD_COLOR);
+
+
+            // TODO: replace with proper error raising
+            if (this->img.empty())
             {
-                std::cout << "Could not read image: " << image_name << std::endl;
-                return 1;
+                std::cout << "Could not read image: " << im_name << std::endl;
             }
 
-            outImg = Mat(inImg.rows, inImg.cols, CV_8UC3, Scalar(0, 0, 0));
+            // outImg = Mat(inImg.rows, inImg.cols, CV_8UC3, Scalar(0, 0, 0));
 
-            // compress 2 bools to single variable
-            if (average && white)
-                mode = 3;
-            else if (average && ! white)
-                mode = 2;
-            else if (!average && white)
-                mode = 1;
-            else
-                mode = 0;
+            this->white = average;
+            this->white = white;
+        }
+
+        void write(std::string name)
+        {
+            imwrite(name, img);
+        }
+
+        void show()
+        {
+
+            //ensures the window is always placed in the top left corner and the name can be reused later in the program
+            String winName = "image";
+            namedWindow(winName, WINDOW_AUTOSIZE);
+            moveWindow(winName, 0, 0);
+
+            imshow(winName, img);
         }
 
         void pixelate()
         {
             // TODO: write without double for loop
             // parallise using #pragma omp parallel for
-            for (int x = 0; x < inImg.cols - blockW; x += blockW)
+            for (int x = 0; x < this->img.cols - blockW; x += blockW)
             {
-                for (int y = 0; y < inImg.rows - blockH; y += blockH)
+                for (int y = 0; y < this->img.rows - blockH; y += blockH)
                 {
                     // cycle through all channels (necessary?)
-                    //for (int c = 0; c < 3; c++)
-                    //{
-
-                        if (stoi(argv[3]) == 1)
+                    for (int c = 0; c < 3; c++)
+                    {
+                        if (average)
                         {
-                            setBlock(getBrightest(img, x, y, blockW, blockH, c), x, y, blockW, blockH, c, newImg);
+                            setBlock(getBrightest(x, y), x, y);
                         }
                         else
                         {
                             //gets the average of the pixel block and then sets all the pixels in that block to that value
-                            setBlock(getAverage(img, x, y, blockW, blockH, c), x, y, blockW, blockH, c, newImg);
+                            setBlock(getAverage(x, y), x, y);
                         }
                     }
                 }
             }
-
         }
 
-        void setBlock(int value, int startW, int startH, int blockW, int blockH, int channel, Mat &img, float increase = 1.0)
+
+    private:
+        void setBlock(Scalar value, int startW, int startH, float increase = 1.0)
         {
             for (int w = 0; w < blockW; w++)
             {
                 for (int h = 0; h < blockH; h++)
                 {
-                    if (value > 255)
+                    // Loop over Scalar and check value within min/max
+                    // warning is restricted to 4 which only holds for Scalar data type
+                    for (short i = 0; i < 4; i++)
                     {
-                        value = 255;
+                        if (value[i] > 255)
+                            value[i] = 255;
+                        else if (value[i] < 0)
+                            value[i] = 0;
                     }
-                    else if (value < 0)
-                    {
-                        value = 0;
-                    }
-                    img.at<Vec3b>(startH + h, startW + w)[channel] = value * increase;
+                    img.at<Scalar>(startH + h, startW + w) = value.mul(increase);
                 }
             }
         }
 
-        int getBrightest()
+        Scalar getBrightest(int x, int y)
         {
-            int brightest = Scalar(0, 0, 0);
+            Scalar brightest = Scalar(0, 0, 0);
             for (int x = 0; x < blockW; x++)
             {
                 for (int y = 0; y < blockH; y++)
                 {
-                    int current = img.at<Vec3b>(height + y, widths + x)[channel];
-                    if (current > brightest)
+                    Scalar current = this->img.at<Scalar>(blockH + y, blockW + x);
+                    for (int i = 0; i < 3; i++)
                     {
-                        brightest = current;
+                        if (current[i] > brightest[i])
+                        {
+                            brightest = current;
+                        }
                     }
                 }
             }
-            // TODO
-            // call getAverage if brightest is white(?)
-            return brightest;
+            int aver = 0;
+
+            if (white)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                if (brightest[i] > 250)
+                    aver++;
+                else
+                    continue;
+                }
+            }
+            
+            
+            if (aver == 3)
+                return (getAverage(x, y));
+            else
+                return brightest;
         }
 
-        int getAverage()
+        // TODO: look at matrix manipulations to avoid double for loops
+        Scalar getAverage(int x, int y)
         {
-            int total = Scalar(0, 0, 0);
+            Scalar total = Scalar(0, 0, 0);
             for (int x = 0; x < blockW; x++)
             {
                 for (int y = 0; y < blockH; y++)
                 {
-                    total += img.at<Vec3b>(height + y, widths + x)[channel];
+                    total = total + this->img.at<Scalar>(blockH + y, blockW + x);
                 }
             }
-            int area = blockH * blockW;
-            int average = (total / area);
+            Scalar average = (total / (blockH * blockW));
             return average;
         }
-
 };
 
 int main(int argc, char *argv[])
@@ -132,38 +164,39 @@ int main(int argc, char *argv[])
      argv[4]: image to be pixelled
      argv[5]: (optional) ouptut file
      */
-
+    std::cout << "Run\n";
     // TODO
     // replace with proper argument flags and processing
     if (argc < 5)
     {
-        cout << "Error not enough arguments\n1: the height of the pixelling blocks\n2: the width of the pixelling blocks\n3: use brightest pixel (1) or average (any other value)\n4: path of image to be pixelled\n5: (optional) file path of output, if non-specified the image will just be displayed\n";
+        std::cout << "Error not enough arguments\n1: the height of the pixelling blocks\n2: the width of the pixelling blocks\n3: use brightest pixel (1) or average (any other value)\n4: path of image to be pixelled\n5: (optional) file path of output, if non-specified the image will just be displayed\n";
         return 2;
     }
-    int blockH = stoi(argv[1]);
-    int blockW = stoi(argv[2]);
 
+    String name = "tmp.jpg";
+    bool average = true, white=false;
+    int blockH = std::stoi(argv[1]);
+    int blockW = std::stoi(argv[2]);
+
+    pixMat img = pixMat(name, blockH, blockW, average, white);
+    // pixMat img2 = static_cast<pixMat>(imread(name, IMREAD_COLOR));
     //int height = (img.rows / blockH) * blockH;
     //int widths = (img.cols / blockW) * blockW;
 
+
     if (argc >= 6)
     {
-        string fileName(argv[5]);
+        String fileName(argv[5]);
         //fileName += ".png";
-        imwrite(fileName, newImg);
+        img.write(fileName);
     }
     else
     {
-        //ensures the window is always placed in the top left corner and the name can be reused later in the program
-        char winName[] = "image";
-        namedWindow(winName, WINDOW_AUTOSIZE);
-        moveWindow(winName, 0, 0);
-
-        imshow(winName, img);
+        img.show();
+        img.pixelate();
         waitKey(0);
-        imshow(winName, newImg);
+        img.show();
         waitKey(0);
-        imwrite("wtest.png", newImg);
     }
     return 0;
 }
